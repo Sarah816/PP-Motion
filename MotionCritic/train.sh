24e56b7e6dfbbb9cac13a8377ffcf78bacb93166
Sarah Zhao <springssarah@hotmail.com>
2025-03-15 15:23:20 +0800

    fix training code

--- MotionCritic/train.sh
+++ MotionCritic/train.sh
@@ -1,4 +1,4 @@
-python train.py --gpu_indices 3,4 --exp_name critic_mdmfull_seed3407 \
-    --dataset mdm \
-    --learning_rate 2e-3 --batch_size 64 --seed 3407\
+python train.py --gpu_indices 4,6 --exp_name critic_overlap_lr3.5e-5 \
+    --dataset overlap \
+    --learning_rate 3.5e-5 --batch_size 32 --seed 3407\
     --save_latest --lr_decay --big_model
